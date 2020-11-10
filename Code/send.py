de2b3c3691a6590674f17c89b04c1486e7f8beb8
Leonardo Henrique da Silva Paixão <42840902+LeohsPaixao@users.noreply.github.com>
2020-11-10 08:18:49 -0300

    Update send.py

--- Code/send.py
+++ Code/send.py
@@ -9,18 +9,18 @@ client = mqtt.Client()
 client.connect(broker_url, broker_port)
 
 try:
-    peso = 50.00
+    peso = 100.00
     while True:
-        limite = randint(0, 25)
+        limite = randint(0, 30)
         print("limite: ", limite)
         if peso <= limite:
-            reposicao = randint(0, 50)
-            while peso + reposicao > 50.00:
-                reposicao = randint(0, 50.00)
+            reposicao = randint(0, 100)
+            while peso + reposicao > 100.00:
+                reposicao = randint(0, 100.00)
                 
             peso += reposicao
 
-        quantidade = float(round(peso / 0.10))
+        quantidade = float(round(peso /(1/50)))
 
         for i in range(5):
 
@@ -32,8 +32,7 @@ try:
             print("Quantidade: " , quantidade)
 
             sleep(3)
-        peso -= float(randint(1.00, 5.00))
+        peso -= randint(2.00, 15.00)
 
 except KeyboardInterrupt:
     print("\nCtrl+C pressionado, encerrando aplicacao e saindo...")
-
